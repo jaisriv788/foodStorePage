b2534d815ffc515a5f6ea753ec3e97bd4a8bb51f
Jai Srivastava <131600482+jaisriv788@users.noreply.github.com>
2024-01-23 23:19:20 +0530

    Update Content.jsx

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -49,7 +49,7 @@ function Content() {
       </div>
       <div className="mt-3 mx-3">
         <div className="text-xl font-bold ml-16">Pizzas(4)</div>
-        <div className="flex flex-col xl:flex-wrap xl:justify-evenly mt-2">
+        <div className="flex flex-col xl:flex-row xl:flex-wrap xl:justify-evenly mt-2">
           <Card data={pizza} />
           <Card data={pizza} />
           <Card data={pizza} />
@@ -58,7 +58,7 @@ function Content() {
       </div>
       <div className="mt-3 mx-3">
         <div className="text-xl font-bold ml-16">Drinks(4)</div>
-        <div className="flex flex-col xl:flex-wrap xl:justify-evenly mt-2">
+        <div className="flex flex-col xl:flex-row xl:flex-wrap xl:justify-evenly mt-2">
           <Card data={drinks} />
           <Card data={drinks} />
           <Card data={drinks} />

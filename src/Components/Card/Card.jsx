07746f78027c5d4425ed20ec8d61f679cf945887
Jai Srivastava <131600482+jaisriv788@users.noreply.github.com>
2024-01-23 22:57:26 +0530

    Update Card.jsx

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -41,7 +41,7 @@ function Card({ data }) {
                 checked={data.star == 5 ? true : false}
               />
             </div>
-            <div className="lg:text-xs lg:pt-1 hidden">{data.rating}</div>
+            <div className="text-xs pt-1 hidden lg:block">{data.rating}</div>
           </div>
           <div className="font-bold mr-3">{data.price}</div>
         </div>
